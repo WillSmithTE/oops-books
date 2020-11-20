c30283da1c9e37fe2ad4bb808567a9dda1588f58
William Smith <williamsmith@Services-MacBook-Pro.local>
2020-11-20 15:38:11 +1100

    actually fix footer hopefully

--- src/Footer.js
+++ src/Footer.js
@@ -3,7 +3,9 @@ import { VERSION_NUMBER } from './util/constants';
 
 export const Footer = () => {
     return <footer className='footer'>
-        <div className='version'>v{VERSION_NUMBER}</div>
-        <div className='email'>willsmithte@gmail.com</div>
+        <div className='footerFlex'>
+            <div className='version'>v{VERSION_NUMBER}</div>
+            <div className='email'>willsmithte@gmail.com</div>
+        </div>
     </footer>;
 };
